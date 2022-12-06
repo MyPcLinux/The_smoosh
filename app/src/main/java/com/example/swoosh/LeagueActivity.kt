76e81c1afe16bef5223e027175b33ee25369b9b6
PC_RUS <progressor1985@gmail.com>
2022-12-06 17:03:12 +0100

    lesson 3

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.example.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LegaueActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_legaue)
